65279c79ebb90fdb83855f6976d04d10ae9eba74
SergiuDeveloper <sergiu.nistor@fenrir.info.uaic.ro>
2020-03-14 16:16:42 +0200

    Minor changes to PredictionApp

--- PredictionApp/predict.py
+++ PredictionApp/predict.py
@@ -1,7 +1,7 @@
 import unemployed_data as ud;
 
 input_document_format = './Data/medii-somaj-%s-%d.csv';
-epochs = 100000;
+epochs = 10000;
 
 plot_folder = './Plots/';
 output_folder = './Output/';
--- PredictionApp/unemployed_data.py
+++ PredictionApp/unemployed_data.py
@@ -1,5 +1,5 @@
 import re;
-import pandas as pd; 
+import pandas as pd;
 import linear_regression as lr;
 
 class UnemployedData:
