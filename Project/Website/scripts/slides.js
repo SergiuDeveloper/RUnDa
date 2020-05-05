124c85440ac52fbb7b7fd31ec18cd2d262b3216a
G2Gmycatisonfire <49059319+G2Gmycatisonfire@users.noreply.github.com>
2020-05-05 19:57:06 +0300

    Fixed Slides

--- Project/Website/scripts/slides.js
+++ Project/Website/scripts/slides.js
@@ -1,5 +1,6 @@
 let slideIndex = 1;
 let timeout;
+let ignoreSleep = false;
 showSlides(slideIndex);
 
 function plusSlide(n){
@@ -11,12 +12,39 @@ function currentSlide(n){
     clearTimeout(timeout);
     showSlides(slideIndex = n);
 }
+/*
+function onMouseOverSlides(){
+    console.log("m over");
+    ignoreSleep = true;
+    timeout = null;
+}
 
+function onMouseLeaveSlides(){
+    console.log("m left");
+    ignoreSleep = false;
+    showSlides(slideIndex);
+}
+*/
 function showSlides(n){
+    //if(ignoreSleep)
+       // return;
     let i;
-    //var slideDescriptions = document.getElementsByClassName("slideshowDescription");
     const slides = document.getElementsByClassName("slideContainer");
     const dots = document.getElementsByClassName("dotSlide");
+    const slideDescriptions = document.getElementsByClassName("slideshowDescription");
+
+    /*for(i = 0; i < slides.length; i++){
+        slides[i].addEventListener("mouseenter", onMouseOverSlides);
+        slides[i].addEventListener("mouseleave", onMouseLeaveSlides);
+    }
+
+    for(i = 0; i < slideDescriptions.length; i++){
+        slideDescriptions[i].addEventListener("mouseenter", onMouseOverSlides);
+        slideDescriptions[i].addEventListener("mouseleave", onMouseLeaveSlides);
+    }*/
+
+    //console.log(slideDescriptions.toString());
+
     if(n > slides.length) {
         slideIndex = 1;
     }
@@ -26,7 +54,7 @@ function showSlides(n){
 
     for(i = 0; i < slides.length; i++){
         slides[i].style.display = "none";
-        //slideDescriptions[i].style.display = "none";
+        slideDescriptions[i].style.display = "none";
     }
 
     for(i = 0; i < dots.length; i++){
@@ -34,7 +62,7 @@ function showSlides(n){
     }
 
     slides[slideIndex-1].style.display = "block";
-    //slideDescriptions[slideIndex-1].style.display = "block";
+    slideDescriptions[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 
     slideIndex++;
@@ -42,5 +70,6 @@ function showSlides(n){
         slideIndex = 1;
     }
 
-    timeout = setTimeout(showSlides, 2000);
+    //if(ignoreSleep === false)
+    timeout = setTimeout(showSlides, 5000);
 }
