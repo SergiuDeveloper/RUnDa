8a63bd4d1eb55f065f611bdd0f5c906f0f6497a8
G2Gmycatisonfire <vlad.loghin00@gmail.com>
2020-06-07 21:06:54 +0300

    Populate Script

--- Project/Website/WebServer/scripts/graph/loadCategories.js
+++ Project/Website/WebServer/scripts/graph/loadCategories.js
@@ -6,61 +6,76 @@ let selectRegressionType = document.getElementById("selectRegressionType");
 let URL = 'https://unemploymentpredictionapi.azurewebsites.net/RetrieveDataCategories';
 
 let categories = [];
+let subcategories = [];
+let locations = [];
+let regressions = [];
 
 let predictionAppCategoriesJSON;
 let predictionAppSubcategoriesJSON;
+let predictionAppLocationsJSON;
+let predictionAppRegressionsJSON;
+
+function selectedSubcategory(){
+    let subcategory = subcategories[selectSubcategory.selectedIndex];
+    Object.keys(predictionAppSubcategoriesJSON).forEach(function (key) {
+        if( key === subcategory )
+            predictionAppLocationsJSON = predictionAppSubcategoriesJSON[key];
+    });
+    locations = populateSelect(selectLocation, predictionAppLocationsJSON, selectedLocation);
+    selectedLocation();
+}
+
+function selectedRegression(){
+
+}
+
+function selectedLocation(){
+    let location = locations[selectLocation.selectedIndex];
+
+    regressions = []; let i = 0;
+
+    Object.values(predictionAppLocationsJSON[location]).forEach(function (value) {
+        regressions[i++] = value;
+        let regression = document.createElement("option");
+        regression.text = value.toString();
+        selectRegressionType.add(regression);
+    })
+}
+
+function populateSelect(selectElement, JSON, onSelectCallback){
+    let i = 0;
+    let categoryArray = [];
+
+    Object.keys(JSON).forEach(function (key) {
+        let category = document.createElement("option");
+        categoryArray[i++] = key;
+        category.text = key.toLowerCase().charAt(0).toUpperCase() + key.toLowerCase().slice(1);
+        selectElement.add(category);
+    })
+
+    selectElement.addEventListener("change", onSelectCallback);
+
+    return categoryArray;
+}
 
 function callbackPopulateCategory(response){
     predictionAppCategoriesJSON = JSON.parse(response).DataAttributes;
-    let i = 0;
 
-    Object.keys(predictionAppCategoriesJSON).forEach(function(key){
-        // console.log(key);
-        let category = document.createElement("option");
-        categories[i++]=key;
-        category.text = key.toLowerCase().charAt(0).toUpperCase() + key.toLowerCase().slice(1);
-        // category.onselect = selectedCategory();
-        // category.addEventListener("select", selectedCategory);
-        selectCategory.add(category);
-    });
-
-    selectCategory.addEventListener("change", selectedCategory);
+    categories = populateSelect(selectCategory, predictionAppCategoriesJSON, selectedCategory);
+    selectedCategory();
 }
 
 function selectedCategory(){
     let category = categories[selectCategory.selectedIndex];
-
     Object.keys(predictionAppCategoriesJSON).forEach(function(key ) {
-        // console.log(category[key]);
-
-        // console.log(predictionAppCategoriesJSON[key]);
-
-        if (key === category) {
+        if (key === category)
             predictionAppSubcategoriesJSON = predictionAppCategoriesJSON[key];
-        }
     });
 
-    // console.log(predictionAppSubcategoriesJSON);
-
-    Object.keys(predictionAppSubcategoriesJSON).forEach(function (key) {
-        let subcategory = document.createElement("option");
-        subcategory[i++]=key;
-        subcategory.text = key.toLowerCase().charAt(0).toUpperCase() + key.toLowerCase().slice(1);
-        // category.onselect = selectedCategory();
-        // category.addEventListener("select", selectedCategory);
-        selectSubcategory.add(subcategory);
-    })
-
-    selectSubcategory.addEventListener("select", selectedSubcategory);
+    subcategories = populateSelect(selectSubcategory, predictionAppSubcategoriesJSON, selectedSubcategory);
+    selectedSubcategory();
 }
 
-function populateSelect(){
-
-}
-
-function selectedSubcategory(){
-
-}
 
 function errorCallbackDebug(state, status, response){
     console.log(state.toString());
