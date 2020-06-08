8d456f64dd7721e05d545cdad609412e6ec68ed8
anaidde <59266338+anaidde@users.noreply.github.com>
2020-06-09 00:30:41 +0300

    Update dataExportXML.php

--- Project/Website/WebServer/dataExportXML.php
+++ Project/Website/WebServer/dataExportXML.php
@@ -1,12 +1,47 @@
 <?php
-header('Content-type: application/json');
-header('Content-Disposition: attachment; filename=UnemploymentData.json');
+//header('Content-type: application/json');
+//header('Content-Disposition: attachment; filename=UnemploymentData.json');
 
-$jsonText = json_decode(file_get_contents('http://predictionappapi2.azurewebsites.net/RetrieveData'), true);
+$jsonData = json_decode(file_get_contents('http://unemploymentpredictionapi.azurewebsites.net/RetrieveData'), true);
 
-$xmlText = new SimpleXMLElement('<root/>');
-array_walk_recursive($jsonText, array($xmlText, 'addChild'));
-print_r ($xmlText->asXML());
+function parseJSONToXMLRec($array){
+    $stringValue = '';
+
+    if(is_array($array)){
+        foreach($array as $key => $value){
+            if(strchr($key, ' '))
+                str_replace(' ', '_', $key);
+
+            if(is_array($value)){
+                $stringValue = $stringValue .  '<' . $key . '>' . parseJSONToXMLRec($value) . '</' . $key . '>';
+            }
+            else {
+                $stringValue = $stringValue .  '<' . $key . '>' . $value . '</' . $key . '>';
+            }
+
+        }
+    }
+
+    return $stringValue;
+}
+//
+//$test_array = array (
+//    'bla' => 'blub',
+//    'foo' => 'bar',
+//    'another_array' => array (
+//        'stack' => 'overflow',
+//    ),
+//);
+
+//$jsonData = $test_array;
+
+echo parseJSONToXMLRec($jsonData);
+
+//
+//
+//$xml = new SimpleXMLElement('<root/>');
+//array_walk_recursive($test_array, array ($xml, 'addChild'));
+//print $xml->asXML();
 ?>
 
 
