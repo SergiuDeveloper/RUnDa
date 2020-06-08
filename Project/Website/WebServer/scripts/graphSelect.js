5f2ba6b745b3b18abd12c7309732e815f620f29b
SergiuDev <sergiu.nistor@info.uaic.ro>
2020-06-08 04:21:02 +0300

    Updated canvas styling(width, height, with respect to the viewport width/height)

--- Project/Website/WebServer/scripts/graphSelect.js
+++ Project/Website/WebServer/scripts/graphSelect.js
@@ -96,7 +96,7 @@ function renderChart(chartType, regressionType) {
     chartObject = new Chart(canvasContext, chartOptions);
 
     canvasContext.canvas.style.width = '90vw';
-    canvasContext.canvas.style.height = '66.5vh';
+    canvasContext.canvas.style.height = '65vh';
     canvasContext.canvas.style.margin = 'auto';
 
     currentChartType = chartType;
