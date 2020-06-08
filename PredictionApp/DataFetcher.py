9822d37bbe7e2b4510707a6ac6e039abc5d04f5b
SergiuDeveloper <sergiudev@hotmail.com>
2020-06-08 09:28:40 +0300

    Modified locations format

--- PredictionApp/DataFetcher.py
+++ PredictionApp/DataFetcher.py
@@ -90,7 +90,7 @@ class DataFetcher():
 
             try:
                 with open(f'Data/{category_name}/{year}/{month}/Data.csv', 'w', encoding = 'latin-1') as csv_file:
-                    csv_file.write(csv_online_content.text)
+                    csv_file.write(csv_online_content.text.replace('-'. ' '))
             except:
                 with open(f'Data/{category_name}/{year}/{month}/Data.csv', 'wb') as csv_file:
                     csv_file.write(csv_online_content.content)
@@ -99,6 +99,14 @@ class DataFetcher():
                 
                 remove(f'Data/{category_name}/{year}/{month}/Data.csv')
                 rename(f'Data/{category_name}/{year}/{month}/Data2.csv', f'Data/{category_name}/{year}/{month}/Data.csv')
+
+                with open(f'Data/{category_name}/{year}/{month}/Data.csv', 'r') as file :
+                    csv_file_content = file.read()
+
+                csv_file_content = csv_file_content.replace('-', ' ')
+
+                with open(f'Data/{category_name}/{year}/{month}/Data.csv', 'w') as file:
+                    file.write(csv_file_content)
 
     @staticmethod
     def __get_resources_list() -> List[Dict[str, Any]]:
