c12e404f8b1aa61b89d45ab8df83570e01c340cb
anaidde <59266338+anaidde@users.noreply.github.com>
2020-06-08 21:32:39 +0300

    Update dataExportCSV.php

--- Project/Website/WebServer/scripts/dataExportCSV.php
+++ Project/Website/WebServer/scripts/dataExportCSV.php
@@ -2,14 +2,37 @@
 header('Content-type: application/json');
 header('Content-Disposition: attachment; filename=UnemploymentData.json');
 
-echo file_get_contents('http://predictionappapi2.azurewebsites.net/RetrieveData');
+//echo file_get_contents('http://predictionappapi2.azurewebsites.net/RetrieveData');
 
 $jsonDecode = json_decode(file_get_contents('http://predictionappapi2.azurewebsites.net/RetrieveData'), true);
 $csvString = '';
 
-foreach ($jsonDecode as $row){
-    $csvString = $csvString . ', ' . $row;
+$csvFile = 'example.csv';
+$fp = fopen($csvFile, 'w');
+
+foreach ($jsonDecode as $row) {
+    foreach ($row as $row1){
+        foreach ($row1 as $row2){
+            foreach ($row2 as $row3){
+                fputcsv($fp, $row3);
+                print_r($row3);
+            }
+        }
+    }
 }
 
-echo $csvString;
+$csvString = file_get_contents('example.csv');
+
+
+
+//print_r($jsonDecode);
+//foreach ($jsonDecode as $row){
+//    foreach($row as $secondRow){
+//        foreach($secondRow as $thirdRow) {
+//            $csvString = $csvString . ', ' . $thirdRow;
+//        }
+//    }
+//}
+
+//echo $csvString;
 ?>
