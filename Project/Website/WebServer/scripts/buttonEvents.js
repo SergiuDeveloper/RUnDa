78aafcc9613c57b79e52936caf059f4da09084f0
anaidde <59266338+anaidde@users.noreply.github.com>
2020-06-09 15:18:44 +0300

    button events

---
+++ Project/Website/WebServer/scripts/buttonEvents.js
@@ -0,0 +1,5 @@
+function dianaButtonClicked() {
+    dianaInfoEmail = "Email: mihaela.pascariu31@gmail.com";
+    dianaInfoPhone = "Phone: +40745364772";
+    document.getElementById("dia").innerHTML = dianaInfoEmail;
+}
